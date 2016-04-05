74eea90f43413510e77fc3bedb24312022947ce6
Junne <junne.bai@gmail.com>
2016-04-05 11:02:06 +0800

    Initial Commit

---
+++ MixedProgrammingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MixedProgrammingDemo
+//
+//  Created by baijf on 4/5/16.
+//  Copyright © 2016 Junne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MixedProgrammingDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MixedProgrammingDemo
+//
+//  Created by baijf on 4/5/16.
+//  Copyright © 2016 Junne. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
